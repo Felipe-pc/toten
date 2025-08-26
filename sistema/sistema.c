0525b0b4abbe03df46d69494dc6d2e2c52e2f6bd
FELIPE <felipe.santos.santana.013@gmail.com>
2025-08-26 19:26:33 -0300

    Update sistema.c

---
+++ sistema/sistema.c
@@ -0,0 +1,13 @@
+#include <Adrafruit_SSD1306.h>
+Adrafruit_SSD1306 tela(128,64,$wire,-1);
+void setup()
+{
+tela.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+tela.clearDisplay();
+tela.display();
+tela.
+}
+void loop()
+{
+
+}
