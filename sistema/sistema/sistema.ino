ea886846fef6519e998499348c84365364f8dcaa
FELIPE <felipe.santos.santana.013@gmail.com>
2025-08-31 17:42:59 -0300

    Update sistema.ino

--- sistema/sistema/sistema.ino
+++ sistema/sistema/sistema.ino
@@ -1,9 +1,20 @@
+#include<Adafruit_SSD1306.h>
+Adafruit_SSD1306 tela(128,64,&Wire,-1);
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
+tela.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+tela.clearDisplay();
+tela.display();
+tela.setTextSize(1);
+tela.setTextColor(SSD1306_WHITE);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-Serial.print("ola,mundo");
+tela.setCursor(0,0);
+tela.println("ola,mundo");
+tela.setCursor(0,16);
+tela.println("12");
+tela.display();
+delay(5000);
 }
