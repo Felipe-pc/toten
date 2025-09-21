9a61b9c62e5d5e2f5268648efb7d2264c0a1adc5
FELIPE <felipe.santos.santana.013@gmail.com>
2025-09-21 18:08:39 -0300

    update

--- sistema/sistema/sistema.ino
+++ sistema/sistema/sistema.ino
@@ -18,6 +18,96 @@ int grafico(int x,int y,char frase[25],int tamanho=1){
   return 0;
 }
 
+int teclado(){
+while(true){
+  int pin1,pin2,pin3,pin4;
+  delay(20);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(6,LOW);
+  pin1=digitalRead(5);
+  pin2=digitalRead(4);
+  pin3=digitalRead(3);
+  pin4=digitalRead(2);
+  if (pin1 == 1){
+    return 1;
+  }
+  if (pin2== 1){
+    return 2;
+  }
+  if (pin3== 1){
+    return 3;
+  }
+  if (pin4== 1){
+    return 4;
+  }
+  delay(20);
+  digitalWrite(9,LOW);
+  digitalWrite(8,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(6,LOW);
+  pin1=digitalRead(5);
+  pin2=digitalRead(4);
+  pin3=digitalRead(3);
+  pin4=digitalRead(2);
+  if (pin1== 1){
+    return 5;
+  }
+  if (pin2== 1){
+    return 6;
+  }
+  if (pin3== 1){
+    return 7;
+  }
+  if (pin4== 1){
+    return 8;
+  }
+  digitalWrite(9,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(7,HIGH);
+  digitalWrite(6,LOW);
+  delay(20);
+  pin1=digitalRead(5);
+  pin2=digitalRead(4);
+  pin3=digitalRead(3);
+  pin4=digitalRead(2);
+  if (pin1== 1){
+    return 9;
+  }
+  if (pin2== 1){
+    return 10;
+  }
+  if (pin3== 1){
+    return 11;
+  }
+  if (pin4== 1){
+    return 12;
+  }
+  delay(20);
+  digitalWrite(9,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(6,HIGH);
+  pin1=digitalRead(5);
+  pin2=digitalRead(4);
+  pin3=digitalRead(3);
+  pin4=digitalRead(2);
+  if (pin1== 1){
+    return 13;
+  }
+  if (pin2== 1){
+    return 14;
+  }
+  if (pin3== 1){
+    return 15;
+  }
+  if (pin4== 1){
+    return 16;
+  }
+}
+}
+
 void loop() {
 delay(2000);
 grafico(0,0,"funciona",2);
